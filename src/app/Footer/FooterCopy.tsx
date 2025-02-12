f791faaf2cf4ddc74b23ce52afa5a5bab1615bb6
TommyDev69 <sunnevian4life@gmail.com>
2025-02-12 06:13:58 +0100

    update

--- src/app/Footer/FooterCopy.tsx
+++ src/app/Footer/FooterCopy.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function FooterCopy() {
   return (
     <div className='w-[45% flex justify-end'>
-      <p className="text-white font-bold text-[10px] md:text-xl capitalize">
+      <p className="text-white text-center font-bold text-[10px] md:text-xl capitalize">
          &#169; 2025 Sunnevian, all rights reserved
       </p>
     </div>
