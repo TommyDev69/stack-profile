fb876e56e3161ce4943647b93196f0a7a6ff7de7
TommyDev69 <sunnevian4life@gmail.com>
2025-02-12 05:58:19 +0100

    job

--- src/app/Footer/FooterContent.tsx
+++ src/app/Footer/FooterContent.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 const FooterContent = () => {
     const [Footer_Nav, Setfooter] = useState(["support", "privacy policy", "Terms and Conditions"])
     let result = Footer_Nav.map((list, index) =>(
-        <p key={index} className="text-xl text-white">{list}</p>
+        <p key={index} className="md:text-xl text-sm text-white">{list}</p>
     ))
     return (  
         <div className="flex w-[45%] space-x-10 items-center">
