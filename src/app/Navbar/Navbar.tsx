f49b533ca0ce48a79ec582f6ad29ffcc437c653a
TommyDev69 <sunnevian4life@gmail.com>
2025-02-12 06:26:36 +0100

    work

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import NavbarContainer from "./NavbarContainer";
 
 const Navbar = () => {
     return (  
-        <nav  className=" relative  mx-auto px-2 md:px-14 mb-0 ">
+        <nav  className=" relative  mx-auto px-2 md:px-10 mb-0 ">
             <NavbarContainer />
             
         </nav>
