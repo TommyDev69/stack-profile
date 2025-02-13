185236d685bcdada90b6b2b194d1be63c3507e5b
TommyDev69 <sunnevian4life@gmail.com>
2025-02-13 15:32:09 +0100

    UPDATE

--- src/app/Footer/FooterContent.tsx
+++ src/app/Footer/FooterContent.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 const FooterContent = () => {
     const [Footer_Nav, Setfooter] = useState(["support", "privacy policy", "Terms and Conditions"])
     let result = Footer_Nav.map((list, index) =>(
-        <p key={index} className="md:text-[24px] text-[8px] text-white">{list}</p>
+        <p key={index} className="md:text-[18px] text-[8px] text-white">{list}</p>
     ))
     return (  
         <div className="flex  justify-center space-x-10 items-center">
--- src/app/Footer/FooterLogo.tsx
+++ src/app/Footer/FooterLogo.tsx
@@ -3,8 +3,8 @@ import Logo from '../Image/sunnevian logo (1).svg'
 const FooterLogo = () => {
  let Brand = Logo
     return ( 
-        <div className="w-1/3 pl-12 bg-black flex flex-col items-center">
-            <Image src={Brand} alt='logo' className="w-[20%] md:w-[40%] mr-[90%]" />
+        <div className="w-[45%] pl-12 bg-black flex flex-col items-center">
+            <Image src={Brand} alt='logo' className="w-[30%] md:w-[40%] mr-[90%]" />
         </div>
      );
 }
