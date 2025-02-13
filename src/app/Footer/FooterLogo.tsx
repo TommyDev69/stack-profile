fb2fa17408def04ff8a23c4d2e5241a2ee94d054
TommyDev69 <sunnevian4life@gmail.com>
2025-02-13 15:47:24 +0100

    stock

--- src/app/Footer/FooterLogo.tsx
+++ src/app/Footer/FooterLogo.tsx
@@ -3,7 +3,7 @@ import Logo from '../Image/sunnevian logo (1).svg'
 const FooterLogo = () => {
  let Brand = Logo
     return ( 
-        <div className="w-[45%] pl-12 bg-black flex flex-col items-center">
+        <div className="w-[30%] pl-12 bg-black flex flex-col items-center">
             <Image src={Brand} alt='logo' className="wv-[35%] md:w-[40%] mr-[90%]" />
         </div>
      );
