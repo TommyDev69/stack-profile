61e9f7c8f625e9d3314dee20b5a4b4a1db4dc4ae
TommyDev69 <sunnevian4life@gmail.com>
2025-02-13 15:53:17 +0100

    update

--- src/app/Footer/FooterCopy.tsx
+++ src/app/Footer/FooterCopy.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function FooterCopy() {
   return (
-    <div className='w-[4w5%] bg-red-600 flex justify-end'>
+    <div className='w-[4w5%]  flex justify-end'>
       <p className="text-white text-center font-bold text-[8px] md:text-[16px] capitalize">
          &#169; 2025 Sunnevian, all rights reserved
       </p>
--- src/app/Footer/FooterLogo.tsx
+++ src/app/Footer/FooterLogo.tsx
@@ -3,7 +3,7 @@ import Logo from '../Image/sunnevian logo (1).svg'
 const FooterLogo = () => {
  let Brand = Logo
     return ( 
-        <div className="w-[30%] pl-12 bg-black flex flex-col items-center">
+        <div className="w-[30%] pl-12  flex flex-col items-center">
             <Image src={Brand} alt='logo' className=" md:w-[30%] mr-[90%]" />
         </div>
      );
